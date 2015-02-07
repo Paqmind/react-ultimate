7e7e7083a0a58bdef5decec3bd59a08a11efbf47
Ivan Kleshnin <ivan@paqmind.com>
2015-02-07 09:35:49 +0200

    Throw in one more snippet

--- backend/app.js
+++ backend/app.js
@@ -135,3 +135,9 @@ if (app.get('env') == "production") {
   mongo.connect('production',logger); // mongodb initialization
 }
 */
+
+
+/*process.on('uncaughtException', function (err) {
+console.error('uncaughtException:', err.message)
+console.error(err.stack)
+process.exit(1)})*/
