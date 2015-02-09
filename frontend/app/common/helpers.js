ba82d38897d2c49060b9013dc9cf60a8994bc7d4
Ivan Kleshnin <ivan@paqmind.com>
2015-02-09 15:25:36 +0200

    Experimental ES6-style expors

--- frontend/app/common/helpers.js
+++ frontend/app/common/helpers.js
@@ -1,4 +1,4 @@
-exports.getModel = function(models, id) {
+export function getModel(models, id) {
   //return models.reduce(
   //  (acc, robot) => robot.id == id ? robot : acc,
   //  undefined
@@ -10,26 +10,25 @@ exports.getModel = function(models, id) {
     }
   }
   return undefined;
-};
+}
 
-exports.addModel = function(models, model) {
+export function addModel(models, model) {
   return models.push(model);
-};
+}
 
-exports.updateModel = function(models, model) {
+export function updateModel(models, model) {
   for (let i=0; i < models.length; i++) {
     if (models[i].id == model.id) {
       models[i] = model;
       return models;
     }
   }
-};
+}
 
-exports.removeModel = function(models, id) {
+export function removeModel(models, id) {
   return models.filter(model => model.id != id);
-};
+}
 
-exports.maxId = function(models) {
-  console.log(models.map(model => model.id));
+export function maxId(models) {
   return Math.max.apply(Math, models.map(model => model.id));
-};
+}
