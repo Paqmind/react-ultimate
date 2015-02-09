de3504d394dfa4e9ad01ffcfdf4d848a37a83389
Ivan Kleshnin <ivan@paqmind.com>
2015-02-09 15:21:56 +0200

    Update backend API

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,101 +1,60 @@
-var ld      = require("lodash"),
-    express = require("express");
+// IMPORTS =========================================================================================
+let Ld = require("lodash");
+let Express = require("express");
+let CommonHelpers = require("../../common/helpers");
 
-var router = express.Router();
+// EXPORTS =========================================================================================
+let router = Express.Router();
 
-var people = [
-  {
-    id: 1,
-    firstName: "Henrik",
-    lastName: "Joreteg",
-    coolnessFactor: 11
-  },
-  {
-    id: 2,
-    firstName: "Bob",
-    lastName: "Saget",
-    coolnessFactor: 2
-  },
-  {
-    id: 3,
-    firstName: "Larry",
-    lastName: "King",
-    coolnessFactor: 4
-  },
-  {
-    id: 4,
-    firstName: "Diana",
-    lastName: "Ross",
-    coolnessFactor: 6
-  },
-  {
-    id: 5,
-    firstName: "Crazy",
-    lastName: "Dave",
-    coolnessFactor: 8
-  },
-  {
-    id: 6,
-    firstName: "Larry",
-    lastName: "Johannson",
-    coolnessFactor: 4
-  }
-];
+let robots = Ld.range(1, 10).map(CommonHelpers.generateRobot);
 
-var id = 7;
-
-/**
- * @api {get} /robots Get the list of robots
- * @apiName RobotIndex
- * @apiGroup Robot
- * @apiDescription In this case "apiUse" is defined and used.
- * @apiVersion 0.2.0
- * @apiParam (URL) {String} name New name of the user
- * @apiParam (query) {String} name New name of the user
- * @apiError NameEmptyError The name was empty. Minimum of <code>1</code> character is required.
- * @apiSuccess {String} name The robots name
- * @apiSuccess {Number} age Calculated age from Birthday
- * @apiSuccessExample Example data on success:
- * {
- *   name: "Paul",
- *   age: 27
- * }
- */
-router.get("/robots", function (req, res) {
-  res.send(people);
+router.get("/robots/", function(req, res) {
+  console.log(robots);
+  res.status(200);
+  res.send(robots);
 });
 
-router.post("/robots", function (req, res) {
-  var person = req.body;
-  id += 1;
-  person.id = id;
-  people.push(person);
+router.post("/robots/", function(req, res) {
+  let robot = req.body;
+  robot.id = Ld.last(robots).id + 1;
+  robots.push(robot);
   res.status(201);
-  res.send(person);
+  res.send(robot);
 });
 
+// TODO add typecheck for :id (on express level)
 router.get("/robots/:id", function(req, res) {
-  var found = ld.findWhere(people, {id: parseInt(req.params.id + "", 10)});
-  res.status(found ? 200 : 404);
-  res.send(found);
-});
-
-router.delete("/robots/:id", function (req, res) {
-  var found = ld.findWhere(people, {id: parseInt(req.params.id + "", 10)});
-  if (found) {
-    people = ld.without(people, found);
+  var robot = Ld.findWhere(robots, {id: parseInt(req.params.id)});
+  if (robot) {
+    res.status(200);
+    res.send(robot);
+  } else {
+    res.status(404);
   }
-  res.status(found ? 200 : 404);
-  res.send(found);
 });
 
+// TODO add typecheck for :id (on express level)
+router.delete("/robots/:id", function(req, res) {
+  var robot = Ld.findWhere(robots, {id: parseInt(req.params.id)});
+  if (robot) {
+    robots = Ld.without(robots, robot);
+    res.status(200);
+    res.send(robot);
+  } else {
+    res.status(404);
+  }
+});
+
+// TODO add typecheck for :id (on express level)
 router.put("/robots/:id", function(req, res) {
-  var found = ld.findWhere(people, {id: parseInt(req.params.id + "", 10)});
-  if (found) {
-    ld.extend(found, req.body);
+  var robot = Ld.findWhere(robots, {id: parseInt(req.params.id)});
+  if (robot) {
+    Ld.extend(robot, req.body);
+    res.status(200);
+    res.send(robot);
+  } else {
+    res.status(404);
   }
-  res.status(found ? 200 : 404);
-  res.send(found);
 });
 
 module.exports = router;
