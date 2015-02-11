0d22dd188dd0e014f666d57fb8fcd3063568cb7e
Ivan Kleshnin <ivan@paqmind.com>
2015-02-11 10:25:24 +0200

    Fix 404 case for static

--- backend/routes/app.js
+++ backend/routes/app.js
@@ -2,8 +2,12 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("*", function(req, res) {
-  res.render("app.html");
+router.get("*", function(req, res, next) {
+  if (req.path.startsWith("/static/")) {
+    next();
+  } else {
+    res.render("app.html");
+  }
 });
 
 module.exports = router;
