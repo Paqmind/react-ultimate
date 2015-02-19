8dc4de0bfd30553291a73ebb20b60fa9ac60fb83
Ivan Kleshnin <ivan@paqmind.com>
2015-02-19 17:18:36 +0200

    Make mixins folder, not file

--- frontend/app/common/mixins/LensedStateMixin.js
+++ frontend/app/common/mixins/LensedStateMixin.js
@@ -2,7 +2,7 @@ let {Map} = require("immutable");
 let ReactLink = require("react/lib/ReactLink");
 let immutableLens = require("paqmind.data-lens").immutableLens;
 
-exports.LensedStateMixin = {
+let LensedStateMixin = {
   linkState: function(key) {
     let lens = immutableLens(key);
     return new ReactLink(
@@ -13,3 +13,5 @@ exports.LensedStateMixin = {
     );
   }
 };
+
+module.exports = LensedStateMixin;
