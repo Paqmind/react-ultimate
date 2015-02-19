844a91a11d96f050a147b35af7af03fa01f1b8f5
Ivan Kleshnin <ivan@paqmind.com>
2015-02-19 13:12:37 +0200

    Allow overrides in robot generation

--- backend/helpers.js
+++ backend/helpers.js
@@ -2,12 +2,12 @@
 let Faker = require("faker");
 
 // EXPORTS =========================================================================================
-export function generateRobot(id) {
+export function generateRobot(override) {
   // no support to pick name by gender for now in Faker :(
-  return {
+  return Object.assign({
     id: Faker.random.uuid(),
     name: Faker.name.firstName(),
     assemblyDate: Faker.date.between("1970-01-01", "1995-01-01"),
     manufacturer: Faker.random.array_element(["Russia", "USA", "China"]),
-  };
+  }, override);
 }
