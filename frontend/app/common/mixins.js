806b4a0fd1757599a213da0723dd9b9495ff2376
Ivan Kleshnin <ivan@paqmind.com>
2015-02-19 13:13:12 +0200

    Update linkState proto-mixin

--- frontend/app/common/mixins.js
+++ frontend/app/common/mixins.js
@@ -1,5 +1,5 @@
+let {Map} = require("immutable");
 let ReactLink = require("react/lib/ReactLink");
-let ReactStateSetters = require("react/lib/ReactStateSetters");
 let immutableLens = require("paqmind.data-lens").immutableLens;
 
 exports.LensedStateMixin = {
@@ -7,11 +7,9 @@ exports.LensedStateMixin = {
     let lens = immutableLens(key);
     return new ReactLink(
       lens.get(this.state),
-      ReactStateSetters.createStateSetter(this, function(state) {
-        return function(value) {
-          return lens.set(value, state);
-        }
-      })
+      (newValue) => {
+        this.setState(lens.set(this.state, newValue));
+      }
     );
   }
 };
