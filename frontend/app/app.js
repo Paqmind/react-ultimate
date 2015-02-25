90a31734a15998e3efd98f81b550b24dcb20c0ba
Ivan Kleshnin <ivan@paqmind.com>
2015-02-25 17:13:56 +0100

    Resolve multiple imports (circular dependency) via router container (window)

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -11,11 +11,43 @@ Promise.prototype.done = function(onFulfilled, onRejected) {
 
 // IMPORTS =========================================================================================
 let React = require("react");
-let Router = require("./router");
-let RobotStore = require("./robot/stores/robots");
+let ReactRouter = require("react-router");
+let {Route, DefaultRoute, NotFoundRoute, HistoryLocation} = ReactRouter;
 
-// MAIN ============================================================================================
-Router.run(function(Handler, state) {
+// Common components
+let Body = require("./common/components/body");
+let Home = require("./common/components/home");
+let About = require("./common/components/about");
+let NotFound = require("./common/components/not-found");
+
+// Robot components
+let RobotRoot = require("./robot/components/root");
+let RobotIndex = require("./robot/components/index");
+let RobotDetail = require("./robot/components/detail");
+let RobotAdd = require("./robot/components/add");
+let RobotEdit = require("./robot/components/edit");
+
+// ROUTES ==========================================================================================
+let routes = (
+  <Route handler={Body} path="/">
+    <DefaultRoute name="home" handler={Home}/>
+    <Route name="robot" path="/robots/" handler={RobotRoot}>
+      <DefaultRoute name="robot-index" handler={RobotIndex}/>
+      <Route name="robot-add" path="add" handler={RobotAdd}/>
+      <Route name="robot-detail" path=":id" handler={RobotDetail}/>
+      <Route name="robot-edit" path=":id/edit" handler={RobotEdit}/>
+    </Route>
+    <Route name="about" path="/about" handler={About}/>
+    <NotFoundRoute handler={NotFound}/>
+  </Route>
+);
+
+window.router = ReactRouter.create({
+  routes: routes,
+  location: HistoryLocation
+});
+
+window.router.run(function(Handler, state) {
   // you might want to push the state of the router to a
   // store for whatever reason
   // RouterActions.routeChange({routerState: state});
--- frontend/app/router.js
+++ frontend/app/router.js
@@ -1,67 +1,28 @@
 // PREVENT CIRCULAR DEPENDENCY LOCK ================================================================
-let router;
 let proxy = {
   makePath(to, params, query) {
-    return router.makePath(to, params, query);
+    return window.router.makePath(to, params, query);
   },
 
   makeHref(to, params, query) {
-    return router.makeHref(to, params, query);
+    return window.router.makeHref(to, params, query);
   },
 
   transitionTo(to, params, query) {
-    router.transitionTo(to, params, query);
+    window.router.transitionTo(to, params, query);
   },
 
   replaceWith(to, params, query) {
-    router.replaceWith(to, params, query);
+    window.router.replaceWith(to, params, query);
   },
 
   goBack() {
-    router.goBack();
+    window.router.goBack();
   },
 
   run(render) {
-    router.run(render);
+    window.router.run(render);
   }
 };
 
 export default proxy;
-
-// IMPORTS =========================================================================================
-let React = require("react");
-let ReactRouter = require("react-router");
-let {Route, DefaultRoute, NotFoundRoute, HistoryLocation} = ReactRouter;
-
-// Common components
-let Body = require("./common/components/body");
-let Home = require("./common/components/home");
-let About = require("./common/components/about");
-let NotFound = require("./common/components/not-found");
-
-// Robot components
-let RobotRoot = require("./robot/components/root");
-let RobotIndex = require("./robot/components/index");
-let RobotDetail = require("./robot/components/detail");
-let RobotAdd = require("./robot/components/add");
-let RobotEdit = require("./robot/components/edit");
-
-// ROUTES ==========================================================================================
-let routes = (
-  <Route handler={Body} path="/">
-    <DefaultRoute name="home" handler={Home}/>
-    <Route name="robot" path="/robots/" handler={RobotRoot}>
-      <DefaultRoute name="robot-index" handler={RobotIndex}/>
-      <Route name="robot-add" path="add" handler={RobotAdd}/>
-      <Route name="robot-detail" path=":id" handler={RobotDetail}/>
-      <Route name="robot-edit" path=":id/edit" handler={RobotEdit}/>
-    </Route>
-    <Route name="about" path="/about" handler={About}/>
-    <NotFoundRoute handler={NotFound}/>
-  </Route>
-);
-
-router = ReactRouter.create({
-  routes: routes,
-  location: ReactRouter.HistoryLocation
-});
