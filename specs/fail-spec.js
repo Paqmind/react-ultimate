c707cae17ccd2794285b131005e78646add32128
Ivan Kleshnin <ivan@paqmind.com>
2015-02-27 15:30:34 +0100

    Add specs folder (BDD)

---
+++ specs/fail-spec.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+require("object.assign").shim();
+
+describe("This", function() {
+  it("should work", function () {
+    //expect(False).to.be.true;
+  });
+});
