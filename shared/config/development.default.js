1e26cd21c80cb1285b473e33ab82730b18195eef
Ivan Kleshnin <ivan@paqmind.com>
2015-04-14 11:15:07 +0200

    Clarify build process and docs

---
+++ shared/config/development.default.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // HTTP
+  "http-port": 3000,
+  "http-use-etag": false,
+
+  // HOSTS (required in "development" envs)
+  "production-ssh": "xxx@yyy",
+  "production-host": "react-starter.xxx.com",
+  "production-service": "react-starter.xxx.node.service",
+
+  "staging-ssh": "xxx@yyy",
+  "staging-host": "demo.react-starter.xxx.com",
+  "staging-service": "demo.react-starter.xxx.node.service",
+};
