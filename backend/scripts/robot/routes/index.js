200d36f3e43cfef0155a4d7c3cded1d4177ece7f
Ivan Kleshnin <ivan@paqmind.com>
2015-04-15 11:51:37 +0200

    Support pagination at backend

--- backend/scripts/robot/routes/index.js
+++ backend/scripts/robot/routes/index.js
@@ -8,14 +8,17 @@ let robots = require("backend/robot/db");
 
 // ROUTES ==========================================================================================
 router.get("/robots/",
-  createParseQuery({}),
+  createParseQuery({page: {offset: Joi.number(), limit: Joi.number()}}),
   function handler(req, res, cb) {
+    let page = req.query.page || {};
+    let offset = page.offset || 0;
+    let limit = page.limit || 20;
     let response = {
-      data: robots.toList(),
+      data: robots.toList().slice(offset, offset + limit),
       meta: {
         page: {
-          //offset: 1,
-          //limit: 10,
+          offset: offset,
+          limit: limit,
           total: robots.count(),
         }
       }
