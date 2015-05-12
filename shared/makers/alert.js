d398801f2654705cef4bee6fd6228d3c7053e945
Ivan Kleshnin <ivan@paqmind.com>
2015-05-12 13:27:45 +0200

    Update makers

--- shared/makers/alert.js
+++ shared/makers/alert.js
@@ -1,10 +1,11 @@
 // IMPORTS =========================================================================================
+import {merge} from "ramda";
 import Faker from "faker";
 import Alert from "shared/models/Alert";
 
 // EXPORTS =========================================================================================
 export default function makeAlert(manualData) {
-  return Alert(Object.assign({
+  return Alert(merge({
     closable: Faker.random.array_element([false, true]),
     expire: Faker.number.between(0, 5000),
   }, manualData));
--- shared/makers/monster.js
+++ shared/makers/monster.js
@@ -1,11 +1,12 @@
 // IMPORTS =========================================================================================
+import {merge} from "ramda";
 import Faker from "faker";
 import Monster from "shared/models/Monster";
 
 // EXPORTS =========================================================================================
 export default function makeMonster(manualData) {
   // no support to pick name by gender for now in Faker :(
-  return Monster(Object.assign({
+  return Monster(merge({
     name: Faker.name.findName(),
     birthDate: Faker.date.between("1970-01-01", "1995-01-01"),
     citizenship: Faker.random.array_element(["Russia", "USA", "China"]),
