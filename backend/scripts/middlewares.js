580538c20c708cb8fbc049d6e2914c6ff2e7a525
Ivan Kleshnin <ivan@paqmind.com>
2015-05-12 13:25:21 +0200

    Update middleware folder structure

--- backend/scripts/middlewares.js
+++
@@ -1,9 +0,0 @@
-import createParseBody from "./middlewares/parse-body";
-import createParseParams from "./middlewares/parse-params";
-import createParseQuery from "./middlewares/parse-query";
-
-export default {
-  createParseBody,
-  createParseParams,
-  createParseQuery,
-}
---
+++ backend/scripts/middlewares/index.js
@@ -0,0 +1,9 @@
+import createParseBody from "./parse-body";
+import createParseParams from "./parse-params";
+import createParseQuery from "./parse-query";
+
+export default {
+  createParseBody,
+  createParseParams,
+  createParseQuery,
+};
