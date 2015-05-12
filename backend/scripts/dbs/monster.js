a08fe33f0db93a365bb025e625eb1bc998ed0378
Ivan Kleshnin <ivan@paqmind.com>
2015-05-12 12:34:39 +0200

    Update DB makers

--- backend/scripts/dbs/monster.js
+++ backend/scripts/dbs/monster.js
@@ -1,16 +1,14 @@
 // IMPORTS =========================================================================================
-import {sortBy, range} from "lodash";
-import makeMonster from "shared/makers/monster";
+import {range, reduce} from "ramda";
+import makeModel from "shared/makers/monster";
 
-// PSEUDO DB =======================================================================================
-// Create first monster with predefined id separately (useful for tests)
-let monsterHead = makeMonster({id: "7f368fc0-5754-493d-b5f6-b5729fc298f7"});
-let monsterTail = range(1, 42).map(makeMonster);
-let monstersDB = [monsterHead]
-  .concat(sortBy(monsterTail, model => model.name))
-  .reduce((obj, model) => {
-    obj[model.id] = model;
-    return obj;
-  }, {});
+// FAKE DB =========================================================================================
+function makeDB() {
+  return reduce(db => {
+    let model = makeModel();
+    db[model.id] = model;
+    return db;
+  }, {}, range(0, 100));
+}
 
-export default monstersDB;
+export default makeDB();
