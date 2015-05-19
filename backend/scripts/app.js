a56cac337aa47bd24ad42a2fa6e52606c5f03f74
Ivan Kleshnin <ivan@paqmind.com>
2015-05-19 15:34:20 +0200

    Remove unused deps and code

--- backend/scripts/app.js
+++ backend/scripts/app.js
@@ -69,65 +69,6 @@ let server = Http.createServer(app);
 server.on("error", onError);
 server.on("listening", onListening);
 server.listen(Config.get("http-port"));
-//var socketEngine = SocketIO(server);
-
-// SOCKET-IO =======================================================================================
-//import ChildProcess from "child_process";
-//import SocketIO from "socket.io";
-//import SocketIOStream from "socket.io-stream";
-//let exec  = ChildProcess.exec,
-//let spawn = ChildProcess.spawn;
-
-//socketEngine.sockets.on("connection", function (socket) {
-//  console.log("[]-> connection");
-//  var logFile = Config.get("project-dir") + "/log-osx.log";
-//  ss(socket).on("enter", function (stream, data) {
-//    var top = spawn("top", ["-l 0"]);
-//    top.stderr.on("data", function (data) {
-//      console.log("ps stderr: " + data);
-//    });
-//    console.log("[]-> enter:");
-//    console.log(`[]-> piping ${logFile}`);
-//    top.stdout.pipe(stream);
-//    console.log("[]-> enter:", Config.get("project-dir") + "/log-osx.log");
-//    Fs.createReadStream(logFile).pipe(stream);
-//  });
-
-//  ss(socket).emit("streaming", top.stdout);
-//  socket.emit("message", {message: "Welcome to the chat!"});
-
-//  socket.on("message", function (data) {
-//    socket.broadcast.emit("message", {message: data});
-//    socket.emit("message", {message: data});
-//  });
-
-//  socket.on("enter", function (data) {
-//    socket.username = data.username;
-//    socketEngine.sockets.emit("message", {message: data.username + " enters the chat"});
-//  });
-
-//  socket.on("disconnect", function () {
-//    socketEngine.sockets.emit("message", {message: socket.username + " leaves the chat"});
-//  });
-//});
-
-
-// all environments
-//app.set('title', 'Application Title');
-
-// development only
-//if (app.get('env') == "development") {
-//  app.use(Express.errorHandler({ dumpExceptions: true, showStack: true }));
-//  app.set('mongodb_uri', 'mongo://localhost/dev');
-//  mongo.connect('development',logger); // mongodb initialization
-//}
-
-// production only
-//if (app.get('env') == "production") {
-//  app.use(Express.errorHandler());
-//  app.set('mongodb_uri', 'mongo://localhost/prod');
-//  mongo.connect('production',logger); // mongodb initialization
-//}
 
 // HELPERS =========================================================================================
 function onError(error) {
@@ -151,8 +92,3 @@ function onError(error) {
 function onListening() {
   logger.info("Listening on port " + Config.get("http-port"));
 }
-
-//process.on('uncaughtException', function (err) {
-//console.error('uncaughtException:', err.message)
-//console.error(err.stack)
-//process.exit(1)})
