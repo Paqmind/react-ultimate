d365d462e4c3bed94f60a176881e707cdbdea1e8
Ivan Kleshnin <ivan@paqmind.com>
2015-05-20 10:18:30 +0200

    Update About component

--- frontend/scripts/components/page/about.js
+++ frontend/scripts/components/page/about.js
@@ -24,23 +24,23 @@ export default class About extends Component {
               App uses AJAX loaders heavily to avoid "white screen" UX issue.
             </p>
 
-          <h3>Index</h3>
-          <p>
-            Any widget in any frontend application may be either bound or unbound to URL.
-            React Ultimate is no exception. We decided to highlight this lesser-known topic by
-            the very app structure.
-          </p>
-          <p>
-            Index for <a href="/robots">Robots</a> is URL-bound. State is mirrored
-            in URL so it's possible to bookmark and share link to exact page state (offset, filters, sorts...).
-            Namespacing of URL params is not supported, e.g. there can't be two URL-bound paginations on the same page.
-          </p>
-          <p>
-            Index for <a href="/monsters">Monsters</a> is URL-unbound. State is implicit and hidden
-            from user so bookmarking is impossible. On the flip side, such components aren't limited
-            in numbers. Also notice how much more performant is ULR-unbound pagination comparing
-            to the URL-bound one.
-          </p>
+            <h3>Index</h3>
+            <p>
+              Any widget in any frontend application may be either bound or unbound to URL.
+              React Ultimate is no exception. We decided to highlight this lesser-known topic by
+              the very app structure.
+            </p>
+            <p>
+              Index for <a href="/robots">Robots</a> is URL-bound. State is mirrored
+              in URL so it's possible to bookmark and share link to exact page state (offset, filters, sorts...).
+              Namespacing of URL params is not supported, e.g. there can't be two URL-bound paginations on the same page.
+            </p>
+            <p>
+              Index for <a href="/monsters">Monsters</a> is URL-unbound. State is implicit and hidden
+              from user so bookmarking is impossible. On the flip side, such components aren't limited
+              in numbers. Also notice how much more performant is ULR-unbound pagination comparing
+              to the URL-bound one.
+            </p>
 
             <h4>Pagination</h4>
             <p>
@@ -73,9 +73,11 @@ export default class About extends Component {
             </p>
 
             <h3>CRUD</h3>
+            <h4>Validation</h4>
             <p>
-              Architecture: validation rules are shared between Backend and Frontend.
-              User friendly: live validation.
+              Text fields are validated after input is stopped to provide immediate feedback
+              about errors. Validations "on blur" and "on submit" are also available, of course.
+              Validation rules are shared between frontend and backend.
             </p>
 
             <h4>Index Backlinks</h4>
@@ -95,9 +97,8 @@ export default class About extends Component {
 
             <h3>Text Holders</h3>
             <p>
-              Typography goodness. The width of text container is auto regulated to display about 80 character per row.
-              Check this <a href="http://www.pearsonified.com/2012/01/characters-per-line.php">comprehensive article</a>
-              for more information.
+              Typography goodness. The width of text container is auto-regulated to display about 80 character per row.
+              Check this <a href="http://www.pearsonified.com/2012/01/characters-per-line.php">comprehensive article</a> for more information.
             </p>
           </section>
         </TextHolder>
