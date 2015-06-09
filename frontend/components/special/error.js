a2ccaf1da7aee4d357889340757a8bf095d040fe
scabbiaza <scabbiaza@ya.ru>
2015-06-09 21:33:34 +0200

    Fix loading component
    Styles were missed

--- frontend/components/special/error.js
+++ frontend/components/special/error.js
@@ -4,7 +4,7 @@ import React from "react";
 import DocumentTitle from "react-document-title";
 import {Component} from "frontend/components/component";
 
-import "frontend/components/special/special.less";
+import "frontend/components/special/gear.less";
 
 // EXPORTS =========================================================================================
 export default class Error extends Component {
