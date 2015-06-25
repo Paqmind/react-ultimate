b967fae3207e8f9841bd7df5d9791d46f3709a13
Ivan Kleshnin <ivan@paqmind.com>
2015-06-25 12:50:09 +0200

    Update configuration management. Close to 12 Factors

--- shared/config/production.js
+++ shared/config/production.js
@@ -1,7 +1,14 @@
+import Path from "path";
+
+// CONFIG ==========================================================================================
 export default {
   // HTTP
-  "http-port": 8080,
+  "http-port": 80,
   "http-use-etag": true,
+
+  // SMTP
+  "smtp-host": "localhost",
+  "smtp-port": 25,
 
   // MAIL
   "mail-robot": "robot@paqmind.com",
