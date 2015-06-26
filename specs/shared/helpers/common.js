55af248b272c4fe18dd25ff45f4d4865f0e9405a
Ivan Kleshnin <ivan@paqmind.com>
2015-06-26 11:43:07 +0200

    Cleanup outdated test expectations

--- specs/shared/helpers/common.js
+++ specs/shared/helpers/common.js
@@ -11,25 +11,6 @@ describe("chunked()", function () {
 });
 
 describe("filterByAll()", function () {
-  it("should handle undefined values", function () {
-    let filters = {manufacturer: "Russia"};
-    let data = [
-      {id: "6", manufacturer: "Russia"},
-      {id: "5", manufacturer: "USA"},
-      {id: "3", manufacturer: "China"},
-      {id: "2", manufacturer: "USA"},
-      {id: "4", manufacturer: "Russia"},
-      {id: "1", manufacturer: "Russia"},
-      undefined, undefined
-    ];
-    let expectedData = [
-      {id: "6", manufacturer: "Russia"},
-      {id: "4", manufacturer: "Russia"},
-      {id: "1", manufacturer: "Russia"},
-    ];
-    expect(filterByAll(filters, data)).eql(expectedData);
-  });
-
   it("should filter by all arguments", function () {
     let filters = {manufacturer: "Russia", id: "1"};
     let data = [
@@ -40,7 +21,6 @@ describe("filterByAll()", function () {
       {id: "4", manufacturer: "Russia"},
       {id: "1", manufacturer: "Russia"},
       {id: "1", manufacturer: "Xyz"},
-      undefined, undefined
     ];
     let expectedData = [
       {id: "1", manufacturer: "Russia"},
@@ -58,33 +38,6 @@ describe("filterByAll()", function () {
 });
 
 describe("sortByAll()", function () {
-  it("should handle undefined values", function () {
-    let sorts = ["-manufacturer"];
-    let data = [
-      undefined,
-      {id: "6", manufacturer: "Russia"},
-      {id: "5", manufacturer: "USA"},
-      undefined,
-      {id: "3", manufacturer: "China"},
-      {id: "4", manufacturer: "Russia"},
-      {id: "2", manufacturer: "USA"},
-      {id: "1", manufacturer: "Russia"},
-      undefined,
-    ];
-    let expectedData = [
-      undefined,
-      {id: "5", manufacturer: "USA"},
-      {id: "6", manufacturer: "Russia"},
-      undefined,
-      {id: "2", manufacturer: "USA"},
-      {id: "4", manufacturer: "Russia"},
-      {id: "1", manufacturer: "Russia"},
-      {id: "3", manufacturer: "China"},
-      undefined,
-    ];
-    expect(sortByAll(sorts, data)).eql(expectedData);
-  });
-
   it("should handle space instead of + (unencoded)", function () {
     let sorts = [" id"];
     let data = [{id: "2"}, {id: "1"}];
@@ -101,7 +54,6 @@ describe("sortByAll()", function () {
       {id: "2", manufacturer: "USA"},
       {id: "4", manufacturer: "Russia"},
       {id: "1", manufacturer: "Russia"},
-      undefined, undefined
     ];
     let expectedData = [
       {id: "3", manufacturer: "China"},
@@ -110,7 +62,6 @@ describe("sortByAll()", function () {
       {id: "6", manufacturer: "Russia"},
       {id: "2", manufacturer: "USA"},
       {id: "5", manufacturer: "USA"},
-      undefined, undefined
     ];
     expect(sortByAll(sorts, data)).eql(expectedData);
   });
