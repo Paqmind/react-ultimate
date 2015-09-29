897018024fa126497358ce9036c074ce3e6337ea
Ivan Kleshnin <ivan@paqmind.com>
2015-09-29 11:54:54 +0200

    Cleanup: remove extra comments

--- backend/templater.js
+++ backend/templater.js
@@ -1,10 +1,8 @@
 import Path from "path";
 import Nunjucks from "nunjucks";
 
-// CONSTANTS =======================================================================================
 const TEMPLATE_DIR = Path.join(__dirname, "/templates");
 
-// TEMPLATER =======================================================================================
 export default function (app) {
   app.set("views", TEMPLATE_DIR);
   app.set("view engine", "html");
