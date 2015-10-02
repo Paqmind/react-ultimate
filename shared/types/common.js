ddeae7e252d99f5bd8f14a2fafadb40cae0a410b
Ivan Kleshnin <ivan@paqmind.com>
2015-10-02 10:43:08 +0200

    Fix UID regex

--- shared/types/common.js
+++ shared/types/common.js
@@ -1,7 +1,7 @@
 import Tc from "tcomb";
 
 let Uid = Tc.subtype(Tc.String, function (x) {
-  return /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/.test(x);
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(x);
 }, "Uid");
 
 export default {
