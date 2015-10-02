a66e4d0e00d15d623b0dd4c5e671e866edfbdf8e
Ivan Kleshnin <ivan@paqmind.com>
2015-10-02 10:41:02 +0200

    Fix backend API: use public/errors instead of templates

--- backend/middlewares/parse-params.js
+++ backend/middlewares/parse-params.js
@@ -1,7 +1,9 @@
+import Path from "path";
 import {map} from "ramda";
 import {validate} from "tcomb-validation";
 import {PUBLIC_DIR} from "shared/constants";
 import {parseTyped} from "shared/parsers";
+import logger from "backend/logger";
 
 export default function createParseParams(type) {
   if (!type) { throw Error("`type` is required"); }
@@ -11,9 +13,10 @@ export default function createParseParams(type) {
     if (result.isValid()) {
       return cb();
     } else {
-      return res.status(400).render("errors/400.html", {
-        errors: map(e => e.message, result.errors)
-      });
+      if (process.env.NODE_ENV != "testing") {
+        logger.error(result.errors);
+      }
+      return res.status(400).sendFile(Path.join(PUBLIC_DIR, "errors/400.html"));
     }
   };
 }
