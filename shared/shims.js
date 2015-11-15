0d4ed889ce1552d383b137cb3cdce9b2c7bdd1a4
Ivan Kleshnin <ivan@paqmind.com>
2015-11-15 12:35:43 +0200

    Define required Node & NPM version. Use new features of Node 4.x

--- shared/shims.js
+++ shared/shims.js
@@ -5,19 +5,8 @@ RegExp.escape = function(string) {
   return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 };
 
-// TODO investigate this crap further
-// let process = process || undefined;
-//if (process) {
-  // IoJS has `unhandledRejection` hook
-  //process.on("unhandledRejection", function (reason, p) {
-  //  throw Error(`UnhandledRejection: ${reason}`);
-  //});
-//} else {
-//  Promise.prototype.done = function done(resolve, reject) {
-//    this
-//      .then(resolve, reject)
-//      .catch(e => {
-//        setTimeout(() => { throw e; }, 0);
-//      });
-//  };
-//}
+if (typeof process == "object") {
+  process.on("unhandledRejection", (reason, promise) => {
+    throw reason;
+  });
+}
