8893ace71a0945718d411e7640243ddc155ee870
Ivan Kleshnin <ivan@paqmind.com>
2015-11-15 12:31:26 +0200

    More exports for Nunjucks templates

--- backend/templater.js
+++ backend/templater.js
@@ -1,5 +1,7 @@
 import Path from "path";
+import Globalize from "globalize";
 import Nunjucks from "nunjucks";
+import constants from "shared/constants";
 
 const TEMPLATE_DIR = Path.join(__dirname, "/templates");
 
@@ -7,8 +9,24 @@ export default function (app) {
   app.set("views", TEMPLATE_DIR);
   app.set("view engine", "html");
 
-  return Nunjucks.configure(TEMPLATE_DIR, {
+  let env = Nunjucks.configure(TEMPLATE_DIR, {
     autoescape: true,
     express: app
   });
+
+  env.addGlobal("String", String);
+  env.addGlobal("Number", Number);
+  env.addGlobal("Object", Object);
+  env.addGlobal("Array", Array);
+  env.addGlobal("RegExp", RegExp);
+  env.addGlobal("Date", Date);
+
+  env.addGlobal("JSON", JSON);
+  env.addGlobal("Math", Math);
+
+  env.addGlobal("Globalize", Globalize);
+
+  env.addGlobal("constants", constants);
+
+  return env;
 }
